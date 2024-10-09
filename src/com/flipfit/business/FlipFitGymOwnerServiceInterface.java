5cf72d240563a135b0d6169625016f0f05f3341e
Deeksha Bisht <Deeksha26bisht.com>
2024-10-09 14:55:55 +0530

    Added new more Business Implementation

--- src/com/flipfit/business/FlipFitGymOwnerServiceInterface.java
+++ src/com/flipfit/business/FlipFitGymOwnerServiceInterface.java
@@ -1,4 +1,12 @@
 package com.flipfit.business;
 
+import com.flipfit.bean.FlipFitGymOwner;
+
 public interface FlipFitGymOwnerServiceInterface {
+    public FlipFitGymOwner register();
+    public boolean gymOwnerLogin(String userName, String password);
+    public void viewAllGymOwners();
+    public String getGymOwnerId(String userName, String password);
+    public void requestGymOwnerApproval(String gymOwnerId);
+
 }
