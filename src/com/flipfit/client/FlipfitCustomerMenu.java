bbc15e6af53b3baed8c739cabf2764c640be64f1
Hariom Gupta <hariom.gupta1@FK1089-226137L.local>
2024-10-09 16:32:52 +0530

    Added customer and gym owner menu

--- src/com/flipfit/client/FlipfitCustomerMenu.java
+++ src/com/flipfit/client/FlipfitCustomerMenu.java
@@ -1,4 +1,31 @@
 package com.flipfit.client;
 
 public class FlipfitCustomerMenu {
+    public void customerLogin(String userName, String password){
+        System.out.println("Customer Login");
+    }
+
+    public void register(){
+        System.out.println("Customer Registered");
+    }
+
+    private void bookSlotSubMenu(String userName){
+        System.out.println("Book Slot Sub Menu");
+    }
+
+    private void cancelBookingSubMenu(String userName){
+        System.out.println("Cancel Booking Sub Menu");
+    }
+
+    private void printUserPlan(String userName){
+        System.out.println("Here is the User Plan");
+    }
+
+    public void customerChangePassword(String userName, String old_password, String new_password){
+        System.out.println("Customer Change Password");
+    }
+
+    public void customerClientMainPage(String username, String customerId){
+        System.out.println("Customer Client Main Page");
+    }
 }
