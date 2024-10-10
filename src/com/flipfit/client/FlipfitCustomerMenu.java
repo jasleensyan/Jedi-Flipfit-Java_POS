7c797fe69dc51c9de48f9bb20d43e614d7b43b3e
Swetha Theresa George <swetha.t@flipkart.com>
2024-10-10 09:52:06 +0530

    added customer menu

--- src/com/flipfit/client/FlipfitCustomerMenu.java
+++ src/com/flipfit/client/FlipfitCustomerMenu.java
@@ -1,6 +1,18 @@
 package com.flipfit.client;
 
+import com.flipfit.bean.FlipFitCustomer;
+import com.flipfit.business.FlipFitCustomerService;
+import com.flipfit.business.FlipFitCustomerServiceInterface;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+import static com.flipfit.constant.Constants.INVALID_CHOICE_ERROR;
+import static com.flipfit.constant.Constants.PREVIOUS_MENU_MESSAGE;
+
 public class FlipfitCustomerMenu {
+    public static Scanner scanner = new Scanner(System.in);
+    FlipFitCustomerServiceInterface flipFitCustomerService = new FlipFitCustomerService();
     public void customerLogin(String userName, String password){
         System.out.println("Customer Login");
     }
@@ -25,7 +37,54 @@ public class FlipfitCustomerMenu {
         System.out.println("Customer Change Password");
     }
 
+    public void printCustomerProfile(FlipFitCustomer customer) {
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("USER ID: "+ customer.getUserID());
+        System.out.println("USER NAME: "+ customer.getUserName());
+        System.out.println("EMAIL: "+  customer.getEmail());
+        System.out.println("CONTACT: "+  customer.getCustomerPhone());
+        System.out.println("CARD DETAILS: "+  customer.getCardDetails());
+        System.out.println("---------------------------------------------------------------------------");
+    }
+
     public void customerClientMainPage(String username, String customerId){
-        System.out.println("Customer Client Main Page");
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("Welcome to customer main page!!");
+        LocalDateTime currentTime = LocalDateTime.now();
+        System.out.println("Login TIME: "+currentTime);
+        System.out.println("---------------------------------------------------------------------------");
+
+        while(true) {
+            System.out.println("1. View Profile \n2. Book a slot in Gym \n3. View Bookings\n4. Cancel Bookings\n5. Go Back to previous menu");
+            System.out.println("---------------------------------------------------------------------------");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    FlipFitCustomer customerProfile = flipFitCustomerService.viewMyProfile(customerId);
+                    printCustomerProfile(customerProfile);
+                    break;
+
+                case 2:
+                    System.out.println("Viewing all Available slots");
+                    bookSlotSubMenu(username);
+                    break;
+
+                case 3:
+                    printUserPlan(username);
+                    break;
+
+                case 4:
+                    cancelBookingSubMenu(username);
+                    break;
+
+                case 5:
+                    System.out.println(PREVIOUS_MENU_MESSAGE);
+                    return;
+
+                default:
+                    System.out.println(INVALID_CHOICE_ERROR);
+                    break;
+            }
+        }
     }
 }
