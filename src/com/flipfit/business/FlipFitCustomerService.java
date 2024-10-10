c0fcd5ca3c52af58305a8313f0dd49ef48a64314
jasleen.kaursyan <jasleensyan@gmail.com>
2024-10-10 12:43:17 +0530

    added customer connectivity with BL

--- src/com/flipfit/business/FlipFitCustomerService.java
+++ src/com/flipfit/business/FlipFitCustomerService.java
@@ -1,8 +1,8 @@
 package com.flipfit.business;
 
-import com.flipfit.bean.Booking;
+//import com.flipfit.bean.Booking;
 import com.flipfit.bean.FlipFitCustomer;
-import com.flipfit.bean.FlipFitGymCentre;
+//import com.flipfit.bean.FlipFitGymCentre;
 import com.flipfit.exception.InvalidCredentialsException;
 
 import java.sql.Date;
@@ -10,20 +10,20 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public class FlipFitCustomerService implements FlipFitCustomerServiceInterface{
-    @Override
-    public List<FlipFitGymCentre> getAllGymCenterDetailsByCity(String city) {
-        return List.of();
-    }
-
-    @Override
-    public List<Booking> getCustomerBookings(String customerId) {
-        return List.of();
-    }
-
-    @Override
-    public boolean bookSlot(String userID, Date date, String slotId, String centreId) {
-        return false;
-    }
+//    @Override
+//    public List<FlipFitGymCentre> getAllGymCenterDetailsByCity(String city) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Booking> getCustomerBookings(String customerId) {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean bookSlot(String userID, Date date, String slotId, String centreId) {
+//        return false;
+//    }
 
     @Override
     public void cancelBooking(String bookingID) {
@@ -44,6 +44,34 @@ public class FlipFitCustomerService implements FlipFitCustomerServiceInterface{
     public FlipFitCustomer viewMyProfile(String customerId) {
         return null;
     }
+
+    @Override
+    public void viewMyProfile(FlipFitCustomer customer) {
+        if(customer == null){System.out.println("USER NAME doesnot exist "); return ;}
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("USER ID: "+ customer.getUserID());
+        System.out.println("USER NAME: "+ customer.getUserName());
+        System.out.println("EMAIL: "+  customer.getEmail());
+        System.out.println("CONTACT: "+  customer.getCustomerPhone());
+        System.out.println("CARD DETAILS: "+  customer.getCardDetails());
+        System.out.println("---------------------------------------------------------------------------");
+    }
+
+
+//    @Override
+//    public FlipFitCustomer viewMyProfile(String  customer) {
+////        if(customer == null){System.out.println("USER NAME doesnot exist "); return null;}
+////        System.out.println("------------------------------------------------------------------------");
+////        System.out.println("USER ID: "+ customer.getUserID());
+////        System.out.println("USER NAME: "+ customer.getUserName());
+////        System.out.println("EMAIL: "+  customer.getEmail());
+////        System.out.println("CONTACT: "+  customer.getCustomerPhone());
+////        System.out.println("CARD DETAILS: "+  customer.getCardDetails());
+////        System.out.println("---------------------------------------------------------------------------");
+//        return null;
+//    }
+
+
 
     @Override
     public String getSlotIdFromGymCentreAndTimestamp(String gymOwner, Timestamp timestamp) {
--- src/com/flipfit/client/FlipfitCustomerMenu.java
+++ src/com/flipfit/client/FlipfitCustomerMenu.java
@@ -13,7 +13,7 @@ import static com.flipfit.constant.Constants.PREVIOUS_MENU_MESSAGE;
 public class FlipfitCustomerMenu {
     public static Scanner scanner = new Scanner(System.in);
     FlipFitCustomerServiceInterface flipFitCustomerService = new FlipFitCustomerService();
-
+    FlipFitCustomer customer = new FlipFitCustomer();
 
 
     public void customerLogin(String userName, String password){
@@ -40,24 +40,13 @@ public class FlipfitCustomerMenu {
         System.out.println("Customer Change Password");
     }
 
-    public void printCustomerProfile(String username) {
-        FlipFitCustomer customer =flipFitCustomerService.viewMyProfile(username);
-        if(customer == null){System.out.println("USER NAME: "+username);return;}
-        System.out.println("------------------------------------------------------------------------");
-        System.out.println("USER ID: "+ customer.getUserID()==null?"null":customer.getUserID());
-        System.out.println("USER NAME: "+ customer.getUserName()==null?"null":customer.getUserName());
-        System.out.println("EMAIL: "+  customer.getEmail()==null?"null":customer.getEmail());
-        System.out.println("CONTACT: "+  customer.getCustomerPhone()==null?"null":customer.getCustomerPhone());
-        System.out.println("CARD DETAILS: "+  customer.getCardDetails()==null?"null":customer.getCardDetails());
-        System.out.println("---------------------------------------------------------------------------");
-    }
-
     public void customerClientMainPage(String username, String customerId){
         System.out.println("---------------------------------------------------------------------------");
         System.out.println("Welcome to customer main page!!");
         LocalDateTime currentTime = LocalDateTime.now();
         System.out.println("Login TIME: "+currentTime);
         System.out.println("---------------------------------------------------------------------------");
+        this.customer= new FlipFitCustomer("1",username,"email@email.com", "password","000999000","aaabbbccc");
 
         while(true) {
             System.out.println("1. View Profile \n2. Book a slot in Gym \n3. View Bookings\n4. Cancel Bookings\n5. Go Back to previous menu");
@@ -65,8 +54,7 @@ public class FlipfitCustomerMenu {
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    FlipFitCustomer customerProfile = flipFitCustomerService.viewMyProfile(customerId);
-                    printCustomerProfile(username);
+                    flipFitCustomerService.viewMyProfile(customer);
                     break;
 
                 case 2:
