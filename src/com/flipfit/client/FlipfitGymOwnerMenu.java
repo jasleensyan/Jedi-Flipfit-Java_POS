b6f64f4628e53036740ac477f75abf2662696f7c
Hariom Gupta <hariom.gupta1@FK1089-226137L.local>
2024-10-10 12:37:33 +0530

    updated gym owner menu

--- src/com/flipfit/client/FlipfitGymOwnerMenu.java
+++ src/com/flipfit/client/FlipfitGymOwnerMenu.java
@@ -1,57 +1,58 @@
 package com.flipfit.client;
 
 import java.util.Scanner;
+import java.util.Date;
 
-import static com.flipfit.constant.Constants.EXIT_MESSAGE;
-import static com.flipfit.constant.Constants.INVALID_CHOICE_ERROR;
+import static com.flipfit.constant.Constants.*;
+import com.flipfit.business.FlipFitGymOwnerService;
+import com.flipfit.bean.Slot;
 
 public class FlipfitGymOwnerMenu {
     public static Scanner scanner = new Scanner(System.in);
+    FlipFitGymOwnerService gymOwnerService = new FlipFitGymOwnerService();
 
     public void gymOwnerLogin(String userName, String password){
         System.out.println("Gym Owner Login");
     }
 
-    public void register(){
-        System.out.println("Register gym owner");
+    public void registerNewGym(){
+        System.out.println("Register new gym center");
     }
 
     public void gymOwnerChangePassword(String userName,String old_password,String new_password){
         System.out.println("Gym Owner Change Password");
     }
 
-    public void addSlot(){
-        System.out.println("Slot Added");
-    }
-
     public void gymOwnerClientMainPage(){
         System.out.println("Welcome to gym owner main page!!");
         System.out.println("---------------------------------------------------------------------------");
         System.out.println("" +
-                "0. View all Gym Centres\n" +
-                "1. Send Gym Owner Approval Request\n" +
-                "2. Add new Gym Center\n" +
-                "3. Request Gym Centre Approval\n" +
-                "4. Add Slots to Gym Centre\n"
+                "1. View all Gym Centers\n" +
+                "2. Send Gym Owner Approval Request\n" +
+                "3. Add new Gym Center\n" +
+                "4. Add Slots to Gym Centre\n" +
+                "5. Go Back to previous menu"
         );
         System.out.println("---------------------------------------------------------------------------");
         int choice = scanner.nextInt();
         switch (choice) {
             case 1:
-                // View All gym centers
+                gymOwnerService.viewAllGymOwners();
                 break;
             case 2:
-                //send gym owner approval request
+                gymOwnerService.requestGymOwnerApproval("123");
                 break;
             case 3:
-                //Add new gym center
+                registerNewGym();
                 break;
             case 4:
-                //Request Gym center Approval
+                Date date = new Date();
+                Slot slot = new Slot("slot123", "center123", date);
+                gymOwnerService.addSlot(slot);
                 break;
             case 5:
-                //Add Slots to gym center
-                break;
+                System.out.println(PREVIOUS_MENU_MESSAGE);
+                return;
             default:
                 System.out.println(INVALID_CHOICE_ERROR);
                 break;
